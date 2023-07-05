99e8dc49f8f134501acf278d73e5c4d4ddb0dc07
WichoGZF <wichogzf@gmail.com>
2023-07-05 10:07:37 -0600

    Excercise 4

---
+++ src/chapter_1/E4.java
@@ -0,0 +1,10 @@
+package chapter_1;
+
+/* Write a program that prints the smallest and largest positive double
+values. Hint: Look up Math.nextUp in the Java API.*/
+public class E4 {
+    public static void main(String[] args){
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
